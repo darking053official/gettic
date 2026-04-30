b88d288f89d3c2e7275832eb68b387191e6db6c9
Darking053 <darking053@protonmail.com>
2026-04-30 19:49:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const dotenv = require('dotenv');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
-
-dotenv.config();
 
 const app = express();
 
