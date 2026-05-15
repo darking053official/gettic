1983fa5e9b0eaf0ecd59fe5a3f29c4ffec92c269
Darking053 <darking053@protonmail.com>
2026-05-15 18:43:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -89,9 +89,6 @@ app.get('/app', (req, res) => {
 // 👇 app içindeki statik dosyalar için (style.css, vs.)
 app.use('/app', express.static(path.join(__dirname, 'app')));
 
-// Statik dosyalar
-app.use(express.static(path.join(__dirname)));
-
 // MongoDB Schema'lar
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true, trim: true, minlength: 3 },
@@ -420,6 +417,9 @@ io.on('connection', (socket) => {
         console.log('Socket ayrildi:', socket.id);
     });
 });
+
+// Statik dosyalar
+app.use(express.static(path.join(__dirname)));
 
 // ==================== 404 SAYFASI ====================
 app.use((req, res) => {
