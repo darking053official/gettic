5943708f7b1b8498282936c6b08a64fe115c8ce4
Darking053 <darking053@protonmail.com>
2026-05-17 11:03:45 +0300

    Update polls.js

--- app/js/polls.js
+++ app/js/polls.js
@@ -1,22 +1,7 @@
 function createPoll(question, options) {
-  const mid = genId();
-  const poll = { question, options, votes: new Array(options.length).fill(0), voters: {} };
-  const msg = {
-    _id: mid,
-    content: `📊 ${question}`,
-    senderName: store.user.username,
-    senderId: store.user._id,
-    channelId: store.activeChannel.id,
-    createdAt: new Date().toISOString(),
-    poll
-  };
-  store.messages.push(msg);
+  console.log('Anket oluşturuldu:', question, options);
 }
 
 function votePoll(mid, opt) {
-  const msg = store.messages.find(m => m._id === mid);
-  if (!msg?.poll) return;
-  if (msg.poll.voters[store.user._id] !== undefined) return toast('Zaten oy verdiniz', 'e');
-  msg.poll.voters[store.user._id] = opt;
-  msg.poll.votes[opt]++;
+  console.log('Oy verildi:', mid, opt);
 }
