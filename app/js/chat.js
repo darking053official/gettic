e6bf1491baaff4007c541e983217b9a98a7728c7
Darking053 <darking053@protonmail.com>
2026-05-17 11:02:19 +0300

    Update chat.js

--- app/js/chat.js
+++ app/js/chat.js
@@ -5,21 +5,5 @@ function formatTime(d) {
 
 function formatMsg(t) {
   if (!t) return '';
-  return t.replace(/\*\*(.+?)\*\*/g, '<b>$1</b>').replace(/\*(.+?)\*/g, '<i>$1</i>');
-}
-
-function createMessageElement(msg, isOwn) {
-  const div = document.createElement('div');
-  div.className = 'msg';
-  div.innerHTML = `
-    <div class="msg-av">${msg.senderName?.charAt(0)?.toUpperCase() || '?'}</div>
-    <div class="msg-body">
-      <div class="msg-head">
-        <span>${msg.senderName || '?'}</span>
-        <span class="msg-time">${formatTime(msg.createdAt)}</span>
-      </div>
-      <div class="msg-text">${formatMsg(msg.content)}</div>
-    </div>
-  `;
-  return div;
+  return t.replace(/\*\*(.+?)\*\*/g, '<b>$1</b>').replace(/\*(.+?)\*/g, '<i>$1</i>').replace(/`([^`]+?)`/g, '<code>$1</code>');
 }
