7a35c91da5721bba439a4fe69f1518c573367fc0
Darking053 <darking053@protonmail.com>
2026-05-18 18:45:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const io = new Server(httpServer, {
     cors: { origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'] }
 });
 
+// gzip
+const compression = require('compression');
+app.use(compression());
+
 // ==================== MIDDLEWARE ====================
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
