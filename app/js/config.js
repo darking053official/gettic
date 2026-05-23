165d1cd4d39543a2d43f9b6af8abee54a0fbd169
Darking053 <darking053@protonmail.com>
2026-05-23 10:24:14 +0300

    Update API endpoint in config.js

--- app/js/config.js
+++ app/js/config.js
@@ -1,3 +1,3 @@
-const API = 'https://gettic-7.tailc20df2.ts.net/';
+const API = 'https://paintball-tiffany-rand-tender.trycloudflare.com';
 const MAX_MSGS = 100;
 function genId() { return Date.now().toString(36) + Math.random().toString(36).substr(2, 7); }
